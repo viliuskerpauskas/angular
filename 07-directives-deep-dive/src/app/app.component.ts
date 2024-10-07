2b0da7b13c3905641283e832a054a731086d67f6
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-10-07 15:58:07 +0300

    159. Host Directives & Composition

--- 07-directives-deep-dive/src/app/app.component.ts
+++ 07-directives-deep-dive/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component, computed, inject } from '@angular/core';
 
-import { AuthComponent } from './auth/auth.component';
-import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
-import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
+import { AuthComponent } from './auth/auth.component';
 import { AuthDirective } from './auth/auth.directive';
+import { AuthService } from './auth/auth.service';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    NgIf,
+    AuthDirective,
+    LogDirective,
+  ],
 })
 export class AppComponent {
   private authService = inject(AuthService);
