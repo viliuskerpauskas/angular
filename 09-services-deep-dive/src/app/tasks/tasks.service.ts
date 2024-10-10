dd663d5f2855219f302ddbd5e6d4f8d02dae72b1
viliuskerpauskas <126024911+viliuskerpauskas@users.noreply.github.com>
2024-10-10 16:50:08 +0300

    177. Using The Alternative Dependency Injection Syntax

--- 09-services-deep-dive/src/app/tasks/tasks.service.ts
+++ 09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -5,7 +5,8 @@ import { Task } from './task.model';
   providedIn: 'root',
 })
 export class TasksService {
-  tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
 
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
