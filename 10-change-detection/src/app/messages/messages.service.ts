2062fd426c8376634a78f1e13d3a655b6cd4857d
viliuskerpauskas <126024911+viliuskerpauskas@users.noreply.github.com>
2024-10-11 12:30:16 +0300

    200. The Problem With OnPush, Cross-Component Data & Not Using Signals

--- 10-change-detection/src/app/messages/messages.service.ts
+++ 10-change-detection/src/app/messages/messages.service.ts
@@ -4,10 +4,15 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class MessagesService {
-  private messages = signal<string[]>([]);
-  allMessages = this.messages.asReadonly();
+  //   private messages = signal<string[]>([]);
+  //   allMessages = this.messages.asReadonly();
+  private messages: string[] = [];
+  get allMessages() {
+    return [...this.messages];
+  }
 
   addMessage(message: string) {
-    this.messages.update((prevMessages) => [...prevMessages, message]);
+    // this.messages.update((prevMessages) => [...prevMessages, message]);
+    this.messages = [...this.messages, message];
   }
 }
--- 10-change-detection/src/app/messages/new-message/new-message.component.ts
+++ 10-change-detection/src/app/messages/new-message/new-message.component.ts
@@ -17,7 +17,8 @@ import { MessagesService } from '../messages.service';
 })
 export class NewMessageComponent {
   private messagesService = inject(MessagesService);
-  enteredText = signal('');
+  // enteredText = signal('');
+  enteredText = '';
 
   get debugOutput() {
     console.log('[NewMessage] "debugOutput" binding re-evaluated.');
@@ -25,7 +26,9 @@ export class NewMessageComponent {
   }
 
   onSubmit() {
-    this.messagesService.addMessage(this.enteredText());
-    this.enteredText.set('');
+    // this.messagesService.addMessage(this.enteredText());
+    // this.enteredText.set('');
+    this.messagesService.addMessage(this.enteredText);
+    this.enteredText = '';
   }
 }
