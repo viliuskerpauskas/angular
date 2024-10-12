75faa787c28b4b2208066e55467ef413888a6036
viliuskerpauskas <126024911+viliuskerpauskas@users.noreply.github.com>
2024-10-12 17:02:48 +0300

    212. Deep Dive: Creating & Using A Custom Observable From Scratch

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   signal,
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +20,19 @@ export class AppComponent implements OnInit {
   clickCount$ = toObservable(this.clickCount);
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({ message: 'New value' });
+      timesExecuted++;
+    }, 2000);
+  });
 
   constructor() {
     // effect(() => {
@@ -38,6 +51,14 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
+    this.customInterval$.subscribe({
+      next: (val) => {
+        console.log(val);
+      },
+      complete: () => {
+        console.log('COMPLETED');
+      },
+    });
     const subscription = this.clickCount$.subscribe({
       next: (val) => {
         console.log(`Clicked button ${this.clickCount()} times.`);
