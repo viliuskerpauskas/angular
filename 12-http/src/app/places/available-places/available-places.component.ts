d62504f8d28de29304dd6278853462d0da64d140
viliuskerpauskas <126024911+viliuskerpauskas@users.noreply.github.com>
2024-10-14 19:16:02 +0300

    225. Sending Data To A Backend

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -51,4 +51,16 @@ export class AvailablePlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: selectedPlace.id,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+      });
+  }
 }
