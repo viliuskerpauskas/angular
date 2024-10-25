6f0eadb1b1f1640a7290093bfe7c9f03cf1872a0
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-10-25 15:34:36 +0300

    248. Syncing Reactive Form Definition & Template

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
   form = new FormGroup({
