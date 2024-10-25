27573815394e1f106d44c5ab25b51c63f4206574
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-10-25 12:12:20 +0300

    236. Introducing HTTP Response Interceptors

--- 12-http/src/main.ts
+++ 12-http/src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(
   request: HttpRequest<unknown>,
@@ -17,7 +19,17 @@ function loggingInterceptor(
   // });
   console.log('[Outgoing Request]');
   console.log(request);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
