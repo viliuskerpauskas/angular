85f0f47e2ffa18920ab1814bd41fdafc9a91d996
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-10-25 15:10:41 +0300

    247. Reactive Forms: Getting Started

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,64 +1,19 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule],
 })
 export class LoginComponent {
-  private destroyRef = inject(DestroyRef);
-  private form = viewChild.required<NgForm>('form');
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-
-      if (savedForm) {
-        const loadedForm = JSON.parse(savedForm);
-        const savedEmail = loadedForm.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-      }
-
-      const subscription = this.form()
-        .valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) => {
-            window.localStorage.setItem(
-              'saved-login-form',
-              JSON.stringify({ email: value.email })
-            );
-          },
-        });
-
-      this.destroyRef.onDestroy(() => {
-        subscription?.unsubscribe();
-      });
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) {
-      return;
-    }
-
-    var enteredEmail = formData.form.value.email;
-    var enteredPassword = formData.form.value.password;
-
-    console.log(formData.form);
-    console.log(enteredEmail, enteredPassword);
-
-    formData.form.reset();
+  onSubmit() {
+    this.form.value.email;
   }
 }
