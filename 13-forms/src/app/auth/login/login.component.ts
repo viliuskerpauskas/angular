2e3586439e175275c9e26f9f0726282ddb27e524
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-10-25 14:08:40 +0300

    239. Template-driven: Registering Form Controls

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
 export class LoginComponent {}
