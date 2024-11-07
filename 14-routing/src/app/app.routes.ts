2fe2f5c02ba849cbba972555bd5aafb870ce0f1c
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-07 17:02:12 +0200

    292. Introducing Route Guards

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,14 +1,22 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import {
   resolveTitle,
   resolveUserName,
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { routes as userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -19,6 +27,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<user-id>
     component: UserTasksComponent,
+    canMatch: [dummyCanMatch],
     children: userRoutes,
     data: {
       message: 'Hello!',
