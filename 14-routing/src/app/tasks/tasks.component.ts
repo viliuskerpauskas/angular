fc4bc545b6bc90dd78f2a93033d90ee8b34325f2
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-07 15:50:41 +0200

    282. Setting Query Parameters

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, inject, input } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
