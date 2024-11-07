3b5fd1ce1bd1ec573a851dcbdb23dcb42de66c54
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-07 17:08:02 +0200

    294. Making Sense of The CanDeactivate Guard

--- 14-routing/src/app/tasks/new-task/new-task.component.ts
+++ 14-routing/src/app/tasks/new-task/new-task.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { TasksService } from '../tasks.service';
-import { Router, RouterLink } from '@angular/router';
+import { CanDeactivateFn, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
@@ -34,3 +34,18 @@ export class NewTaskComponent {
     });
   }
 }
+
+export const canLeaveEditPage: CanDeactivateFn<NewTaskComponent> = (
+  component
+) => {
+  if (
+    component.enteredTitle() ||
+    component.enteredDate() ||
+    component.enteredSummary()
+  ) {
+    return window.confirm(
+      'Do you really want to leave? You will lose the entered data.'
+    );
+  }
+  return true;
+};
