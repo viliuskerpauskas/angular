8b9bfd56e2c9ac1ce9e822e6093bdd752a6e2ec2
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-07 15:26:15 +0200

    275. Accessing Parent Route Data From Inside Nested Routes

--- 14-routing/src/app/app.config.ts
+++ 14-routing/src/app/app.config.ts
@@ -1,7 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
+    ),
+  ],
 };
