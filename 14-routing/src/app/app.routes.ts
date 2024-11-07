7851ef845b216658a70b4f9c6a8df4d8df72c948
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-07 14:24:32 +0200

    266. Registering Multiple Routes

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
   {
+    path: '', // <your-domain>
+    component: NoTaskComponent,
+  },
+  {
     path: 'tasks',
-    component: TasksComponent,
+    component: TasksComponent, // <your-domaind>/tasks
   },
 ];
